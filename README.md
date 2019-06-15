8f4c2662cb62eb2544e0e277fddf02cdb909259e
Cam Saul <cammsaul@gmail.com>
2019-06-14 17:47:41 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Downloads](https://versions.deps.co/metabase/connection-pool/downloads.svg)](https://versions.deps.co/metabase/connection-pool)
+[![Dependencies Status](https://versions.deps.co/metabase/connection-pool/status.svg)](https://versions.deps.co/metabase/connection-pool)
+[![Circle CI](https://circleci.com/gh/metabase/connection-pool.svg?style=svg)](https://circleci.com/gh/metabase/connection-pool)
+[![License](https://img.shields.io/badge/license-Eclipse%20Public%20License-blue.svg)](https://raw.githubusercontent.com/metabase/connection-pool/master/LICENSE)
+[![cljdoc badge](https://cljdoc.org/badge/metabase/connection-pool)](https://cljdoc.org/d/metabase/connection-pool/CURRENT)
+
+[![Clojars Project](https://clojars.org/metabase/connection-pool/latest-version.svg)](http://clojars.org/metabase/connection-pool)
---
+++ project.clj
@@ -0,0 +1,72 @@
+(defproject metabase/connection-pool "1.0.0-SNAPSHOT"
+  :description "Connection pools for JDBC databases. Simple wrapper around C3P0."
+  :url "https://github.com/metabase/connection-pool"
+  :min-lein-version "2.5.0"
+
+  :license {:name "Eclipse Public License"
+            :url "https://raw.githubusercontent.com/metabase/connection-pool/master/LICENSE"}
+
+  :aliases
+  {"test"                      ["with-profile" "+expectations" "expectations"]
+   "bikeshed"                  ["with-profile" "+bikeshed" "bikeshed" "--max-line-length" "120"]
+   "check-namespace-decls"     ["with-profile" "+check-namespace-decls" "check-namespace-decls"]
+   "eastwood"                  ["with-profile" "+eastwood" "eastwood"]
+   "check-reflection-warnings" ["with-profile" "+reflection-warnings" "check"]
+   "docstring-checker"         ["with-profile" "+docstring-checker" "docstring-checker"]
+   ;; `lein lint` will run all linters
+   "lint"                      ["do" ["eastwood"] ["bikeshed"] ["check-namespace-decls"] ["docstring-checker"]]}
+
+  :dependencies
+  [[com.mchange/c3p0 "0.9.5.3"]]
+
+  :profiles
+  {:dev
+   {:dependencies
+    [[org.clojure/clojure "1.10.0"]
+     [expectations "2.2.0-beta2"]]
+
+    :injections
+    [(require 'expectations)
+     ((resolve 'expectations/disable-run-on-shutdown))]
+
+    :jvm-opts
+    ["-Xverify:none"]}
+
+   :expectations
+   {:plugins [[lein-expectations "0.0.8" :exclusions [expectations]]]}
+
+   :eastwood
+   {:plugins
+    [[jonase/eastwood "0.3.5" :exclusions [org.clojure/clojure]]]
+
+    :add-linters
+    [:unused-private-vars
+     :unused-namespaces
+     :unused-fn-args
+     :unused-locals]
+
+    :exclude-linters
+    [:deprecations]}
+
+   :docstring-checker
+   {:plugins
+    [[docstring-checker "1.0.3"]]
+
+    :docstring-checker
+    {:exclude [#"test"]}}
+
+   :bikeshed
+   {:plugins
+    [[lein-bikeshed "0.5.2"]]}
+
+   :check-namespace-decls
+   {:plugins               [[lein-check-namespace-decls "1.0.2"]]
+    :source-paths          ["test"]
+    :check-namespace-decls {:prefix-rewriting true}}}
+
+  :deploy-repositories
+  [["clojars"
+    {:url           "https://clojars.org/repo"
+     :username      :env/clojars_username
+     :password      :env/clojars_password
+     :sign-releases false}]])
