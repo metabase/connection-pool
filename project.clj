b1c0d11f3f249a43e30d2496bbf4a952708e3404
Oleksandr Yakushev <alex@bytopia.org>
2024-12-10 23:46:37 +0200

    Update c3p0 version to 0.10.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metabase/connection-pool "1.2.0"
+(defproject metabase/connection-pool "1.3.0"
   :description "Connection pools for JDBC databases. Simple wrapper around C3P0."
   :url "https://github.com/metabase/connection-pool"
   :min-lein-version "2.5.0"
@@ -15,7 +15,7 @@
    "lint"                      ["do" ["eastwood"] ["bikeshed"] ["check-namespace-decls"] ["docstring-checker"]]}
 
   :dependencies
-  [[com.mchange/c3p0 "0.9.5.5"]]
+  [[com.mchange/c3p0 "0.10.1"]]
 
   :profiles
   {:dev
